3c09fae7a994de103922b4923ce538bc79f713d5
myfullstackprojectAnnaUniversity <myunknownnumber95@gmail.com>
2024-06-19 22:25:01 +0530

    Update Footer.jsx

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -8,27 +8,7 @@ import { FaFacebookF } from 'react-icons/fa';
 const Footer = () => (
   <div className="footer-container">
      <p>2024 Elanchezhiyan.E All rights reserved</p>
-     <p className="icons">
-    <div>
-      <a href="https://www.instagram.com/codeliketheresnotomorrow/" >
-       <BsInstagram />
-      </a>
-    </div> 
-    <div>
-    <a href="https://www.linkedin.com/in/poornashree-thirumalai-6b1b19220/" target="_blank" rel="noreferrer">
-      <BsLinkedin />
-      </a>
-    </div>
-    <div>
-    <a href= "https://www.youtube.com/channel/UCqVcyTRTo_lMkfKCIa3L5HQ" target="_blank" rel="noreferrer">
-    <BsYoutube  />
-      </a>
-    </div>
-    <div>
-    <a href="https://github.com/tpoornashree" target="_blank" rel="noreferrer">
-    <BsGithub  />
-      </a>
-    </div> </p>
+    
   </div>
 );
 
